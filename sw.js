5b6c1eaef4db21bccaca75f7d0d0a344577ba548
Quva <thilakjeevesh@gmail.com>
2026-03-21 11:13:24 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', (event) => {
-    console.log('Service Worker installed');
-});
-self.addEventListener('fetch', (event) => {
-    // Just a pass-through for now
-});
- 
